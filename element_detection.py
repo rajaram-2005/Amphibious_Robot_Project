2b0b5031e4f9763f9c346258c2cc6efdc8b8ae3b
rajaram-2005 <112193954+rajaram-2005@users.noreply.github.com>
2024-09-10 19:29:16 +0530

    Delete element_detection.py

--- element_detection.py
+++
@@ -1,8 +0,0 @@
-# Analyze element data received from Arduino
-def analyze_element_data(element_id):
-    if element_id > 118:
-        print("New element detected! Taking a sample.")
-        return "COLLECT_SAMPLE"
-    else:
-        print(f"Known element detected: {element_id}")
-        return "CONTINUE"
